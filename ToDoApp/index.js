b4e0f9bd4cf29e0281b23a31ba7f6ed79a0a1279
MaryGege <mariamgege04@gmail.com>
2024-10-08 19:37:28 +0400

    Add files via upload

---
+++ ToDoApp/index.js
@@ -0,0 +1,9 @@
+const darkmode = document.getElementById("dark");
+const body = document.querySelector("body");
+const darkBackground = document.querySelector("bodydark");
+
+darkmode.addEventListener("click", ()=>{
+    body.classList.add("dark")
+
+});
+
